4587f1a14baaec4c4204eb22a0fa0d87f7a53b25
Harm Putman <harm.putman@foursites.nl>
2020-01-17 15:17:09 +0100

    init class contstructor

--- src/Init.php
+++ src/Init.php
@@ -6,7 +6,7 @@
  * @copyright:  2020 - High Five
  *
  * Created:     2020-01-17, 02:46:00 pm
- * Modified:    2020-01-17, 02:50:57 pm
+ * Modified:    2020-01-17, 03:15:04 pm
  * Modified By: Harm Putman <harm@high-five.dev>
  */
 
@@ -21,6 +21,8 @@ class Init
     public function __construct()
     {
         $this->setServices();
+
+        return $this;
     }
 
     protected function setServices()
